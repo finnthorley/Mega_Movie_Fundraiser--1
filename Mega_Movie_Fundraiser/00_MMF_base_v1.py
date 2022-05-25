e0a3b1ba3ee163d85aee7edb6e825ead5bf52bbe
19151 <65930349+finnthorley@users.noreply.github.com>
2022-05-25 23:06:03 +0000

    #

---
+++ Mega_Movie_Fundraiser/00_MMF_base_v1.py
@@ -0,0 +1,25 @@
+#import statements 
+
+
+#functions go here
+
+
+#********** Main Routine **********
+
+# Set up dictionaries / lists need to hold data
+
+# Ask user if they have used the program before & show inscructions if necessary
+
+# Loop to get ticket details 
+
+    # Get name (can't be blank)
+    # Get age (between 12 and 130)
+    # Calculate ticket price
+    # Loop to ask for snacks
+    # Calculate snack price
+
+    #ask for payement method (and apply surcharge if nessecary)
+
+# Calculate total sales and profit
+
+# Output data to text file 
