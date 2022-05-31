aacf1cb6a54a80628fbc2dd5b3ffa86fcffff310
19151 <65930349+finnthorley@users.noreply.github.com>
2022-05-31 03:02:30 +0000

    added more

---
+++ Mega_Movie_Fundraiser/02_ticket_loop_v1.py
@@ -0,0 +1,15 @@
+# start of loop
+
+# initialise loop so that it runs at least once
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count <= MAX_TICKETS:
+    print("You have {} seats "
+          "left".format(MAX_TICKETS - count))
+  
+
+    # Get details...
+    name = input("Name: ")
+    count += 1
